e2af6caa8b6d103c506ab92274cd6b0e6e380a08
Maxeem <jmax.posh@gmail.com>
2019-11-19 17:16:19 +0300

    fix broken directory.json url

--- app/src/main/kotlin/maxeem/america/gdg/misc/Conf.kt
+++ app/src/main/kotlin/maxeem/america/gdg/misc/Conf.kt
@@ -8,8 +8,10 @@ object Conf {
 
     object GDG {
 
-        private const val GDG_BASE_URL  = "https://developer.google.com/community/gdg/directory/"
-                const val GET_DIRECTORY = "directory.json"
+        private const val GDG_BASE_URL  = "https://bit.ly/"
+                const val GET_DIRECTORY = "directoryjson"
+//        private const val GDG_BASE_URL  = "https://developer.google.com/community/gdg/directory/"
+//                const val GET_DIRECTORY = "directory.json"
 //        private const val DELAY         = 5_000
 //        private const val DELAY_URL   = "http://slowwly.robertomurray.co.uk/delay/$DELAY/url"
 //        const val API_BASE_URL  = "$DELAY_URL/$GDG_BASE_URL"
