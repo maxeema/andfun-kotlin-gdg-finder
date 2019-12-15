394bcd7cbca65867b8944f74f8ce124839a74f10
Maxeem <jmax.posh@gmail.com>
2019-12-15 09:26:11 +0300

    ask real gdg server for actual data

--- app/src/main/kotlin/maxeem/america/gdg/misc/Conf.kt
+++ app/src/main/kotlin/maxeem/america/gdg/misc/Conf.kt
@@ -8,16 +8,16 @@ object Conf {
 
     object GDG {
 
-        private const val GDG_BASE_URL  = "https://bit.ly/"
-                const val GET_DIRECTORY = "directoryjson"
-//        private const val GDG_BASE_URL  = "https://developer.google.com/community/gdg/directory/"
-//                const val GET_DIRECTORY = "directory.json"
+        private const val GDG_BASE_URL  = "https://developer.google.com/community/gdg/groups/"
+                const val GET_DIRECTORY = "directory.json"
+                const val API_BASE_URL  = GDG_BASE_URL
+//        private const val GDG_BASE_URL  = "https://bit.ly/"
+//                const val GET_DIRECTORY = "directoryjson"
 //        private const val DELAY         = 5_000
 //        private const val DELAY_URL   = "http://slowwly.robertomurray.co.uk/delay/$DELAY/url"
 //        const val API_BASE_URL  = "$DELAY_URL/$GDG_BASE_URL"
-        const val API_BASE_URL  = GDG_BASE_URL
 
-        val REGIONS = arrayOf("Africa", "Asia", "Australia", "Europe", "LATAM", "North America")
+        val REGIONS = arrayOf("Africa", "Asia", "Australia", "Europe", "Middle East", "North America", "South America")
 
     }
 
